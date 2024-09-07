f04e58205bd75d06b224541ef5ae4f64a314e858
Rohan Gajmer <90803596+rohangajmer42@users.noreply.github.com>
2024-09-06 23:18:24 -0500

    Update main.cpp

--- Assignments/P10055/main.cpp
+++ Assignments/P10055/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <cstdlib> // For abs function
+using namespace std;
+
+int main() {
+    long long a, b; // Declare variables to store soldier numbers
+    
+    // Read input until End Of File (EOF)
+    while (cin >> a >> b) {
+        // Output the absolute difference between the two numbers
+        cout << abs(a - b) << endl;
+    }
+
+    return 0;
+}
 
